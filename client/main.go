8fcab3abefa26d94e5575c8f2dd27b8bf7ee0b24
Thomas <Thomas Holvoet>
2019-09-27 21:30:57 +0200

    Implement Exercise 1 of HW1

---
+++ client/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/dedis/protobuf"
+	"net"
+)
+
+var (
+	UIPort 	string
+	msg string
+)
+
+func main() {
+	// Load command line arguments
+	flag.StringVar(&UIPort, "UIPort", "8080", "port for the UI client (default '8080'")
+	flag.StringVar(&msg, "msg", "", "message to be sent")
+
+	flag.Parse()
+
+	// Send message to Peerster
+	sendMsg(msg, UIPort)
+}
+
+// Message format that will be marshalled
+type Message struct {
+	Text string
+}
+
+// Send message to UDP server on localhost:port
+func sendMsg(msg string, port string) {
+	// Set up UDP socket
+	addr := "127.0.0.1" + ":" + port
+	remoteAddr, err := net.ResolveUDPAddr("udp", addr)
+	conn, err := net.DialUDP("udp", nil, remoteAddr)
+
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+		return
+	}
+
+	// Close connection after message is sent
+	defer conn.Close()
+
+	packetBytes, err := protobuf.Encode(&Message{msg})
+	if err != nil {
+		fmt.Printf("ERROR: Could not serialize message\n")
+		fmt.Println(err)
+	}
+
+	n, err := conn.Write(packetBytes)
+
+	if err != nil {
+		fmt.Printf("ERROR: %v\n", err)
+	} else {
+		fmt.Printf("Successfully sent %v bytes to localhost:%v\n", n, port)
+	}
+}
+
